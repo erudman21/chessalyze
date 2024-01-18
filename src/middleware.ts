7a8a3837607e0780ae2c5bc23c0a6a1ff4bb6b51
erudman21 <erudman21@gmail.com>
2024-01-18 10:36:23 -0800

    fix auth redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,28 +6,25 @@ export default withAuth(
   async function middleware(req) {
     const token = await getToken({ req });
     const isAuth = !!token;
+    const isAuthPage = req.nextUrl.pathname.startsWith("/login");
 
-    if (req.nextUrl.pathname === "/login") {
+    if (isAuthPage) {
       if (isAuth) {
-        return NextResponse.redirect(new URL("/", req.url));
+        return NextResponse.redirect(new URL("/evaluate", req.url));
       }
 
       return null;
     }
 
     if (!isAuth) {
-      return NextResponse.redirect(new URL("/login"));
+      return NextResponse.redirect(new URL("/login", req.url));
     }
   },
   {
     callbacks: {
-      async authorized() {
+      authorized() {
         return true;
       },
     },
   }
 );
-
-export const config = {
-  matcher: ["/", "/login", "/evaluate"],
-};
